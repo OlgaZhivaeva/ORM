af07d576056ff588a963263f2892cf86fa85cf75
Olga Zhivaeva <olga.zhiv@yandex.ru>
2023-04-11 00:37:08 +0300

    Added function max_len

--- orm.py
+++ orm.py
@@ -28,17 +28,21 @@ if __name__ == '__main__':
         }[record.get('model')]
         session.add(model(id=record.get('pk'), **record.get('fields')))
     session.commit()
-    def tabulation(name, number):
-        return ' '*(number - len(name))
 
+    def max_len(column):
+        """Функция определяуе максимальную длину строки в столбце"""
+        rows = []
+        for row in session.query(column).all():
+            rows.append(row[0])
+        return len(max(rows, key=len))
 
     while True:
-        publisher_id = int(input('Введите идентификатор издателя или 0 для выхода '))
+        publisher_id = int(input('Введите идентификатор издателя (от 1 до 4) или 0 для выхода '))
         if publisher_id != 0:
             subq = session.query(Book.title, Shop.name, Stock.id).join(Stock.book).join(Stock.shop).filter(Book.id_publisher == publisher_id).subquery()
             for result in session.query(subq.c.title, subq.c.name, Sale.price, Sale.date_sale).join(subq, Sale.id_stock == subq.c.id).all():
-                print(f'{result[0]}', tabulation(result[0], 39), end='| ')
-                print(f'{result[1]}', tabulation(result[1], 8), end='| ')
+                print(result[0], ' '*(max_len(Book.title) - len(result[0])), end='| ')
+                print(result[1], ' '*(max_len(Shop.name) - len(result[1])), end='| ')
                 print(result[2], result[3], sep=' | ')
         else: break
 
